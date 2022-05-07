c5a714e850883e3af49b3b4b8fa842d9cdfa192d
Aleksandr Gontcharov <agontcharov@microsoft.com>
2022-05-07 14:49:19 -0700

    Create the Main of the program

--- SliderGame/Program.cs
+++ SliderGame/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SliderGame.Game;
+
+
+// Welcome Message
+
+
+Console.WriteLine("Welcome to the SliderGame!");
+Console.WriteLine();
+Console.WriteLine("The exciting adventure for pressing buttons!");
+Console.WriteLine("Choose between integers 1 through 4 and try to line up the buttons.");
+Console.WriteLine();
+Console.WriteLine("------------------------------------------------------------------");
+Console.WriteLine("<<<<<<<<<<<<<<<<<LEVEL ONE>>>>>>>>>>>>>>>>>");
+Console.WriteLine("------------------------------------------------------------------");
+
+// Create Stage one
+var slider1 = new Slider(5, 3, false);
+var slider2 = new Slider(7, 4, true);
+var slider3 = new Slider(3, 2, false);
+var slider4 = new Slider(4, 3, true);
+
+
+
+var listOfSliders = new List<Slider>()
+                                                {
+                                                    slider1, slider2, slider3, slider4,
+                                                };
+
+
+var game = new Stage(listOfSliders);
+
+bool didYouWin = false;
+
+while (!didYouWin)
+{
+    // print the current stage
+
+
+    // Ask user for input
+    Console.Write("Next move: ");
+    string userInput = Console.ReadLine();
+
+    // execute stage.ExecuteTurn
+    game.ExecuteTurn(Int32.Parse(userInput));
+
+    // figure out if you won:
+    didYouWin = game.IsWinning();
+
+    // if yes -> go back to line 13
+    // if no --> print the stage and congratulate the played for winning
+}
+// print the current stage
+
+
+Console.WriteLine("Congratulations, you have solved the mystery of SliderGame!!!");
+Console.ReadLine();
