2f698a0d36df13a22272a15ceb1148b146156f24
Aleksandr Gontcharov <agontcharov@microsoft.com>
2022-05-07 14:07:36 -0700

    Implement tests and logic for Slider

--- SliderGame/Game/Slider.cs
+++ SliderGame/Game/Slider.cs
@@ -7,20 +7,60 @@ public class Slider
 
     public Slider(int height, int currPosition, bool direction)
     {
+        constructorHelper(height, currPosition, direction);
+    }
+
+    public Slider(int height)
+    {
+        Random r = new Random();
+        int currPosition = r.Next(0, height); // "random number between 0 and height"
+        constructorHelper(height, currPosition, false);
+    }
+
+    void constructorHelper(int height, int currPosition, bool direction)
+    {
+        if (currPosition >= height)
+        {
+            currPosition = height;
+            direction = false;
+        }
+        else if (currPosition <= 0)
+        {
+            currPosition = 0;
+            direction = true;
+        }
+
         this.height = height;
         this.currPosition = currPosition;
         this.direction = direction;
     }
 
-    public Slider(int height)
-    {
-        this.height = height;
-        this.currPosition = 0;
-        this.direction = true;
-    }
-
     public void Next()
     {
-        throw new NotImplementedException();
+        // edge cases
+        if (currPosition == this.height)
+        {
+            this.currPosition = currPosition - 1;
+            this.constructorHelper(this.height, currPosition, false);
+
+        }
+        else if (currPosition == 0)
+        {
+            this.currPosition = currPosition + 1;
+            this.constructorHelper(this.height, currPosition, true);
+        }
+        else
+        { // in the middle of the slider
+            if (direction)
+            {
+                this.currPosition = currPosition + 1;
+                this.constructorHelper(this.height, currPosition, true);
+            }
+            else
+            {
+                this.currPosition = currPosition - 1;
+                this.constructorHelper(this.height, currPosition, false);
+            }
+        }
     }
 }
--- SliderGameTests/Game/SliderTests.cs
+++ SliderGameTests/Game/SliderTests.cs
@@ -13,13 +13,55 @@ namespace SliderGame.Game.Tests
         [Fact()]
         public void SliderTest()
         {
-            Assert.True(false, "This test needs an implementation");
+            // Test basic functionality of constructor
+            var sliderOne = new Slider(5,3,true);
+
+            Assert.True(sliderOne.height == 5, "The constructor for slider does not assign height correctly");
+            Assert.True(sliderOne.currPosition == 3, "The constructor for slider does not assign currPosition correctly");
+            Assert.True(sliderOne.direction == true, "The constructor for slider does not assign direction correctly");
+
+            // Create objects that are not valid
+
+            var sliderNotValid1 = new Slider(5, 7, true);
+            Assert.True((sliderNotValid1.currPosition == 5 && sliderNotValid1.direction == false), "sliderNotValid1 unexepected result");
+
+            var sliderNotValid2 = new Slider(5, -3,true);
+
+            Assert.True((sliderNotValid2.currPosition == 0 && sliderNotValid2.direction == true), "sliderNotValid2 unexepected result");
+            
+            var sliderNotValid3 = new Slider(5, 5, true);
+            Assert.True((sliderNotValid3.currPosition == 5 && sliderNotValid3.direction == false), "sliderNotValid3 unexepected result");
+            
+            var sliderNotValid4 = new Slider(5, 0, false);
+            Assert.True((sliderNotValid4.currPosition == 0 && sliderNotValid4.direction == true), "sliderNotValid4 unexepected result");
+
+            var sliderNotValid5 = new Slider(5);
+            Assert.True((sliderNotValid5.currPosition <= 5 && sliderNotValid5.currPosition >= 0), "sliderNotValid5 unexepected result");
         }
 
         [Fact()]
         public void NextTest()
         {
-            Assert.True(false, "This test needs an implementation");
+
+            // Test basic functionality of constructor
+            var slider1 = new Slider(5, 3, true);
+            slider1.Next();
+            Assert.True((slider1.currPosition == 4 && slider1.direction == true), "slider1 Incorrect");
+
+
+            var slider2 = new Slider(5, 3, false);
+            slider2.Next();
+            Assert.True((slider2.currPosition == 2 && slider2.direction == false), "slider2 Incorrect");
+
+
+            var slider3 = new Slider(5, 5, false);
+            slider3.Next();
+            Assert.True((slider3.currPosition == 4 && slider3.direction == false), "slider3 Incorrect");
+
+
+            var slider4 = new Slider(5, 0, true);
+            slider4.Next();
+            Assert.True((slider4.currPosition == 1 && slider4.direction == true), "slider4 Incorrect");
         }
     }
 }
